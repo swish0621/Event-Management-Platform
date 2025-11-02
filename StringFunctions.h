7a809ba874bf7fe965d8b007a05d83240c809d35
swish0621 <swish0621@gmail.com>
2025-11-02 15:57:36 -0700

    Add string parsing functions

---
+++ StringFunctions.h
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+
+// removes spaces in the front and back of a item if present 
+std::string stripSpaces(std::string input){
+    if(input.empty()){
+        return input;
+    }
+
+    std::size_t start = 0;
+    std::size_t end = input.size();
+
+    if(input.front() == ' ') { start += 1; }
+    if(input.back() == ' ') { end -= 1; }
+    
+    return input.substr(start, end - start);
+}
+
+// tranforms comma separated strings into a vector of strings
+std::vector<std::string> split(std::string input){
+    std::vector<std::string> store;
+    int i = 0;
+    int start = 0; 
+    int end = 0;
+    for(auto it = input.begin(); it != input.end(); it++, i++){
+        if(*it == ','){
+            store.push_back(stripSpaces(input.substr(start, i - start)));
+            start = i + 1;
+        }
+    }
+    store.push_back(stripSpaces(input.substr(start)));
+
+    return store;
+}

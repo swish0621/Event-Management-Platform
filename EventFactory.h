521b89913fdfd837ca9fa8ef3a5edf432e897579
swish0621 <swish0621@gmail.com>
2025-11-02 13:59:03 -0700

    Removed redundant functions in EventFactory. Object constructors will be used instead.

--- EventFactory.h
+++ EventFactory.h
@@ -9,28 +9,6 @@ class EventFactory {
         EventFactory& operator=(const EventFactory&) = delete;
         EventFactory(const EventFactory&) = delete;
 
-
-        // uses the ConcertEvent constructor to create a new ConcertEvent 
-        Event* createConcertEvent(int id, std::string event_name, double price, int available_tickets, 
-            std::vector<std::string> artists, MusicGenre genre, DateTime event_date);
-        
-        // uses the SportEvent constructor to create a new SportEvent
-        Event* createSportEvent(int id, std::string event_name, double price, int available_tickets,
-            DateTime event_date, std::string away_team, std::string home_team, SportType sport_type);
-        
-        // uses the TheaterEvent constructor to create a new TheaterEvent
-        Event* createTheaterEvent(int id, std::string event_name, double price, int available_tickets, std::string original_title, 
-            std::string director, std::vector<std::string> performers, TheaterGenre genre, bool age_restricted, DateTime date);
-
-        // uses the ConventionEvent constructor to create a new ConventionEvent
-        Event* createConventionEvent(int id, std::string event_name, double price, int available_tickets, 
-            std::string industry_type, int num_exhibitors, std::vector<std::string> exhibitors,
-            std::vector<std::string> sponsors, int num_days, DateTime date);
-
-        // uses the ComedyEvent constructor to create a new ComedyEvent
-        Event* createComedyEvent(int id, std::string event_name, double price, int available_tickets, 
-            std::string performer, bool age_restricted, std::vector<std::string> topics, DateTime date);
-
     public:
         static EventFactory* getInstance() {
             if(instance_ == nullptr) { 

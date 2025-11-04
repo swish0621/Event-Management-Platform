731818921d1bec23eddc889864e05c0ef827f6f0
swish0621 <swish0621@gmail.com>
2025-11-04 10:51:56 -0700

    Define getUser(), addUser() and createUser()

--- EventManager.cpp
+++ EventManager.cpp
@@ -1,8 +1,40 @@
 #include "EventManager.h"
 
-User* EventManager::getUser(int id) const {}
+User* EventManager::getUser(int id) const {
+    auto user = users_.find(id);
+    if(user != users_.end()){
+        return user->second;
+    }
+    return nullptr;
+}
 
-void EventManager::addUser(User* user) {}
+void EventManager::addUser(User* user) {
+    users_[user->getId()] = user;
+}
+
+User* EventManager::createUser(int id) {
+    std::string input;
+    std::string name;
+    std::string s_balance;
+    std::cout << "SELECT A USER TYPE:\n" << "1: ATTENDEE\n2:ORGANIZER" << std::endl;
+    std::cin >> input;
+
+    std::cout << "ENTER YOUR NAME:" << std::endl;
+    std::getline(std::cin, name);
+    std::cout << "ENTER AMOUNT OF FUNDS TO OPEN ACCOUNT WITH (FORMAT XX.XX):" << std::endl;
+    std::cin >> s_balance;
+    double balance = std::stod(s_balance);
+
+    User* user;
+    if(std::stoi(input) == 1) {
+        User* user = new Attendee(id, name, balance);
+    }
+    else {
+        User* user = new Organizer(id, name, balance);
+    }
+    addUser(user);
+    return user; 
+}
 
 void EventManager::createEvent(User* organizer) {}
 

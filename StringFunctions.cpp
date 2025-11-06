97fc349ce24fd04763327f39729f2ff36aeb07db
swish0621 <swish0621@gmail.com>
2025-11-06 16:36:51 -0700

    Move function definitions to cpp file

---
+++ StringFunctions.cpp
@@ -0,0 +1,31 @@
+#include "StringFunctions.h"
+
+
+std::string stripSpaces(std::string input){
+    if(input.empty()){
+        return input;
+    }
+
+    std::size_t start = 0;
+    std::size_t end = input.size();
+
+    if(input.front() == ' ') { start += 1; }
+    if(input.back() == ' ') { end -= 1; }
+    
+    return input.substr(start, end - start);
+}
+
+std::vector<std::string> split(std::string input, char delimiter){
+    std::vector<std::string> store;
+    int i = 0;
+    int start = 0; 
+    for(auto it = input.begin(); it != input.end(); it++, i++){
+        if(*it == delimiter){
+            store.push_back(stripSpaces(input.substr(start, i - start)));
+            start = i + 1;
+        }
+    }
+    store.push_back(stripSpaces(input.substr(start)));
+
+    return store;
+}
--- StringFunctions.h
+++ StringFunctions.h
@@ -1,33 +1,12 @@
+#ifndef STRINGFUNCTIONS_H
+#define STRINGFUNCTIONS_H
 #include <string>
 #include <vector>
 
 // removes spaces in the front and back of a item if present 
-std::string stripSpaces(std::string input){
-    if(input.empty()){
-        return input;
-    }
-
-    std::size_t start = 0;
-    std::size_t end = input.size();
-
-    if(input.front() == ' ') { start += 1; }
-    if(input.back() == ' ') { end -= 1; }
-    
-    return input.substr(start, end - start);
-}
+std::string stripSpaces(std::string input);
 
 // tranforms comma separated strings into a vector of strings
-std::vector<std::string> split(std::string input, char delimiter){
-    std::vector<std::string> store;
-    int i = 0;
-    int start = 0; 
-    for(auto it = input.begin(); it != input.end(); it++, i++){
-        if(*it == delimiter){
-            store.push_back(stripSpaces(input.substr(start, i - start)));
-            start = i + 1;
-        }
-    }
-    store.push_back(stripSpaces(input.substr(start)));
+std::vector<std::string> split(std::string input, char delimiter);
 
-    return store;
-}
+#endif

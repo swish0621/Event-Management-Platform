55e97fcd1a462d6429c8d253b981b16b5ff5c59d
swish0621 <swish0621@gmail.com>
2025-11-06 21:39:59 -0700

    Add Sport test

--- test.cpp
+++ test.cpp
@@ -34,7 +34,38 @@ TEST_CASE("TEST_USER_ORGANIZER_CREATION", "[USER-ATTENDEE]"){
     REQUIRE(user->getRole() == Role::Attendee);
 }
 
-TEST_CASE("TEST_CONCERT_EVENT_CREATION", "[CONCERT]"){
+TEST_CASE("TEST_EVENT_CREATION", "[EVENT]"){
     EventManager* manager = EventManager::getInstance();
     std::istringstream input("2\nJoe Jones\n100\n");
+    std::streambuf* old = std::cin.rdbuf(input.rdbuf());
+    manager->createUser();
+    std::cin.rdbuf(old);
+    User* user = manager->getUser(0);
+    
+    SECTION("CONCERT"){
+        std::istringstream input1("1\nConcert\n99.99\n100\nJake,John,James\n1\n2025\n12\n05\n12\n30\n");
+        std::streambuf* old1 = std::cin.rdbuf(input1.rdbuf());
+        manager->createEvent(user);
+        std::cin.rdbuf(old1);
+
+        Event* event = manager->getEvent(0);
+        REQUIRE(event->getEventName() == "Concert");
+        REQUIRE(event->getOrganizer() == user);
+        REQUIRE(event->getPrice() == 99.99);
+        REQUIRE(event->getAvailableTickets() == 100);
+        REQUIRE(event->getEventCategory() == EventCategory::Concert);
+    }
+    SECTION("SPORT"){
+        std::istringstream input1("2\nSport\n99.99\n100\n2025\n12\n05\n12\n30\nRangers\nCardinals\n8\n");
+        std::streambuf* old1 = std::cin.rdbuf(input1.rdbuf());
+        manager->createEvent(user);
+        std::cin.rdbuf(old1);
+
+        Event* event = manager->getEvent(1);
+        REQUIRE(event->getEventName() == "Sport");
+        REQUIRE(event->getOrganizer() == user);
+        REQUIRE(event->getPrice() == 99.99);
+        REQUIRE(event->getAvailableTickets() == 100);
+        REQUIRE(event->getEventCategory() == EventCategory::Sport);
+    }
 }
